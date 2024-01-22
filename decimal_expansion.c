956944157c06386fd22d92947c7b740a260c82e3
bumblezhou <bumble.zhou@gmail.com>
2024-01-23 07:45:47 +0800

    fix errors

--- decimal_expansion.c
+++ decimal_expansion.c
@@ -17,10 +17,16 @@ void get_decimal_expansion(int *arr, int devident, int devisor, int decimal_plac
     if (decimal_place >= limit) {
         return;
     }
+    
+    devident = 10 * devident;
 
+    int i;
     while (devident < devisor) {
         devident *= 10;
+        arr[i] = 0;
+        i++;
     }
+    decimal_place += i;
 
     double d10 = (double)devident / devisor;
     int mod = devident % devisor;
@@ -32,8 +38,8 @@ void get_decimal_expansion(int *arr, int devident, int devisor, int decimal_plac
     // = mod / devisor
     // = devident % devisor / devisor
     double next_decimal1 = (devident % devisor) / (double)devisor;
-    // printf("devident:%d, devisor:%d, mod: %d, d10:%f, decimal_place:%d, int_part:%d, next_decimal:%f, next_decimal1:%f\n", devident, devisor, mod, d10, decimal_place, int_part, next_decimal, next_decimal1);
-    // printf("next_devident:%d, next_devisor:%d\n", (devident % devisor), devisor);
+    printf("devident:%d, devisor:%d, mod: %d, d10:%f, decimal_place:%d, int_part:%d, next_decimal:%f, next_decimal1:%f\n", devident, devisor, mod, d10, decimal_place, int_part, next_decimal, next_decimal1);
+    printf("next_devident:%d, next_devisor:%d\n", (devident % devisor), devisor);
     get_decimal_expansion(arr, (devident % devisor), devisor, (decimal_place + 1), limit);
 }
 
@@ -41,10 +47,11 @@ void get_decimal_expansion(int *arr, int devident, int devisor, int decimal_plac
 
 int main()
 {
-    int decimal_expansion[EXPANSION_SIZE] = {0};
-    int devident = 11, devisor = 7;
+    int devident = 1, devisor = 3000;
     int quotient = devident/devisor;
-    get_decimal_expansion(decimal_expansion, devident, devisor, 1, EXPANSION_SIZE);
+    
+    int decimal_expansion[EXPANSION_SIZE] = {0};
+    get_decimal_expansion(decimal_expansion, devident, devisor, 1, 100);
     
     printf("%d.", quotient);
     print_int_array(decimal_expansion, EXPANSION_SIZE);
